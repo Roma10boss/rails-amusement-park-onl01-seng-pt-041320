d81743ba9311d1d959779ca43ff272c17116767a
Romario Gustave <romagustave10@gmail.com>
2020-09-27 07:17:06 +0000

    Automatically backed up by Learn

--- spec/features/users_features_spec.rb
+++ spec/features/users_features_spec.rb
@@ -23,6 +23,44 @@ describe 'Feature Test: User Signup', :type => :feature do
 
 
 
+  it 'successfully signs up as admin' do
+    visit '/users/new'
+    expect(current_path).to eq('/users/new')
+    # admin_signup method is defined in login_helper.rb
+    admin_signup
+    expect(current_path).to eq('/users/1')
+    expect(page).to have_content("Walt Disney")
+    expect(page).to have_content("ADMIN")
+  end
+
+  it "on sign up for admin, successfully adds a session hash" do
+    visit '/users/new'
+    # admin_signup method is defined in login_helper.rb
+    admin_signup
+    expect(page.get_rack_session_key('user_id')).to_not be_nil
+  end
+
+  it 'successfully logs in as admin' do
+    create_standard_and_admin_user
+    visit '/signin'
+    expect(current_path).to eq('/signin')
+    # admin_login method is defined in login_helper.rb
+    admin_login
+    expect(current_path).to eq('/users/2')
+    expect(page).to have_content("Walt Disney")
+    expect(page).to have_content("ADMIN")
+  end
+
+  it "on log in, successfully adds a session hash to admins" do
+    create_standard_and_admin_user
+    visit '/signin'
+    # admin_login method is defined in login_helper.rb
+    admin_login
+    expect(page.get_rack_session_key('user_id')).to_not be_nil
+  end
+
+end
+
 describe 'Feature Test: User Signout', :type => :feature do
 
   it 'has a link to log out from the users/show page' do
