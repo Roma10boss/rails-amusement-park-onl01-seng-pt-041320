81c7a9b0748be1b1eb4543496152709f346b5abc
Romario Gustave <romagustave10@gmail.com>
2020-09-27 07:03:20 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,26 @@
 class UsersController < ApplicationController
     before_action :logged_in?, except: [:new, :create]
-
+  
     def new
       @user = User.new
     end
-
+  
     def create
       @user = User.new(user_params)
 
       if @user.save
-
+        
         session[:user_id] = @user.id
         redirect_to user_path(current_user.id)
       else
         render :new
       end
     end
-
+  
     def edit
       @user = current_user
     end
-
+  
     def update
       @user = current_user
       if @user.update(user_params)
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
         render :edit
       end
     end
-
+  
     def show
       @user = User.find(params[:id])
     end
